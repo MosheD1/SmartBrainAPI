d988b329188c20b0699d74ba2c9a3980ab9c4f51
Moshe Dego <moshedego@gmail.com>
2020-03-31 14:48:32 +0300

    Finished back-end for face recognition app

---
+++ Controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'dac78900d5974f92993fda7b7d3b9a16'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries);
+    })
+    .catch(err => res.status(400).json('unable to get enries'));
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
